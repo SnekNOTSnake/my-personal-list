36f2d4bdbb2d5576539d3cad98f685ae0fd04982
Akhmad Najmuddin <udintobe@gmail.com>
2022-03-14 14:47:12 +0700

    Add keyboard shortcuts

--- packages/renderer/src/components/Explorer/Right/RightExplorer.tsx
+++ packages/renderer/src/components/Explorer/Right/RightExplorer.tsx
@@ -1,20 +1,104 @@
 import React, { useEffect } from 'react'
 import { MdSearch, MdOutlineExpandMore } from 'react-icons/md'
 import { useRecoilState, useRecoilValue } from 'recoil'
-import { NavLink, useNavigate } from 'react-router-dom'
+import { NavLink, useNavigate, useParams } from 'react-router-dom'
 
 import { filteredSeries, seriesFilter } from '@/store/series'
 import styles from './RightExplorer.module.css'
 
+const getPath = (path: string) => `/explore/${path}`
+
 const RightExplorer: React.FC = () => {
 	const navigate = useNavigate()
+	const { '*': path } = useParams()
+	const inputRef = React.useRef<HTMLInputElement | null>(null)
+	const titlesRef = React.useRef<HTMLDivElement | null>(null)
 
-	const series = useRecoilValue(filteredSeries)
+	const filtered = useRecoilValue(filteredSeries)
 	const [filter, setFilter] = useRecoilState(seriesFilter)
 
+	// Auto select first occurrence
 	useEffect(() => {
-		if (series.length) navigate(`/explore/${series[0].path}`)
-	}, [])
+		if (filtered.length && !filtered.some((el) => el.path === path))
+			navigate(getPath(filtered[0].path))
+	}, [filter])
+
+	// Shortcuts
+	useEffect(() => {
+		const index = filtered.findIndex((el) => el.path === path)
+
+		const listener = (e: KeyboardEvent) => {
+			const iFocused = document.querySelector(`.${styles.search}:focus`)
+			const focused = document.querySelector(`.${styles.root}:focus-within`)
+			if (!titlesRef.current || !inputRef.current || !focused) return
+
+			const scrollAccordingly = () => {
+				if (!titlesRef.current) return
+
+				const pth = filtered[index - (e.key === 'ArrowUp' ? 1 : -1)].path
+				const next = document.querySelector(
+					`[data-path="${pth}"]`,
+				) as HTMLElement
+
+				const pastBottom =
+					next.offsetTop - 35 >=
+					titlesRef.current.scrollTop + titlesRef.current.offsetHeight
+				const pastTop = next.offsetTop - 70 <= titlesRef.current.scrollTop
+
+				if (pastTop) {
+					titlesRef.current.scrollTo({ top: next.offsetTop - 70 })
+				} else if (pastBottom) {
+					titlesRef.current.scrollTo({
+						top: next.offsetTop - 35 - titlesRef.current.offsetHeight,
+					})
+				}
+			}
+
+			switch (e.key) {
+				case 'ArrowUp':
+					e.preventDefault()
+					if (!filtered[index - 1]) return
+
+					scrollAccordingly()
+					navigate(getPath(filtered[index - 1].path))
+					break
+
+				case 'ArrowDown':
+					e.preventDefault()
+					if (!filtered[index + 1]) return
+
+					scrollAccordingly()
+					navigate(getPath(filtered[index + 1].path))
+					break
+
+				case 'Home':
+					if (!filtered.length || iFocused) return
+					navigate(getPath(filtered[0].path))
+					break
+
+				case 'End':
+					if (!filtered.length || iFocused) return
+					navigate(getPath(filtered[filtered.length - 1].path))
+					break
+
+				case 'f':
+					if (e.ctrlKey) inputRef.current.focus()
+					break
+
+				case 'Escape':
+					inputRef.current.blur()
+					setFilter((prevVal) => ({ ...prevVal, query: '' }))
+					break
+
+				default:
+					console.log(e.key)
+					break
+			}
+		}
+
+		window.addEventListener('keydown', listener)
+		return () => window.removeEventListener('keydown', listener)
+	}, [path, filtered])
 
 	const onSearchChange = (e: InputChange) => {
 		setFilter((prevVal) => ({ ...prevVal, query: e.target.value }))
@@ -38,10 +122,12 @@ const RightExplorer: React.FC = () => {
 			<div className={styles.toolBar}>
 				<div>
 					<input
+						ref={inputRef}
 						type='search'
 						placeholder='Search'
 						value={filter.query}
 						onChange={onSearchChange}
+						className={styles.search}
 					/>
 				</div>
 				<div>
@@ -67,13 +153,13 @@ const RightExplorer: React.FC = () => {
 					/>
 				</div>
 			</div>
-			<div className={styles.titles}>
+			<div ref={titlesRef} className={styles.titles}>
 				<ul>
-					{series.map((el) => (
-						<li key={el.path}>
+					{filtered.map((el) => (
+						<li key={el.path} data-path={el.path}>
 							<NavLink
 								className={({ isActive }) => (isActive ? styles.active : '')}
-								to={`/explore/${encodeURIComponent(el.path)}`}
+								to={`/explore/${encodeURI(el.path)}`}
 							>
 								<span>{el.title || el.path}</span>
 							</NavLink>
@@ -81,7 +167,7 @@ const RightExplorer: React.FC = () => {
 					))}
 				</ul>
 			</div>
-			<div className={styles.matches}>{series.length} Items</div>
+			<div className={styles.matches}>{filtered.length} Items</div>
 		</div>
 	)
 }
