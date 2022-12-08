fbef1867a204679133e499f8d93979c1b8253083
Akhmad Najmuddin <udintobe@gmail.com>
2022-12-09 06:27:50 +0700

    Fix Bug: cannot edit notes

--- packages/renderer/src/pages/Explore/components/AnimeEdit/EditNotes/EditNotes.tsx
+++ packages/renderer/src/pages/Explore/components/AnimeEdit/EditNotes/EditNotes.tsx
@@ -6,7 +6,12 @@ type Props = { notesI: string; onInputChange: (e: TextAreaChange) => any }
 
 const EditNotes: React.FC<Props> = ({ notesI, onInputChange }) => (
 	<div className={styles.editNotes}>
-		<TextArea value={notesI} onChange={onInputChange} label='Notes' />
+		<TextArea
+			value={notesI}
+			onChange={onInputChange}
+			label='Notes'
+			name='notes'
+		/>
 	</div>
 )
 
